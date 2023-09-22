ad6c5208f7d8e7ec5d05bdadb63ca3c5d02af8aa
José Ángel Núñez Gónzaléz <jangelnunezglz@users.noreply.github.com>
2023-09-22 14:48:16 -0600

    upload files

---
+++ UCLed.designer.vb
@@ -0,0 +1,137 @@
+﻿<System.ComponentModel.Designer(GetType(CDesigner))> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UCLed
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UCLed))
+        Me.pbxSupport = New System.Windows.Forms.PictureBox()
+        Me.pbxOnline = New System.Windows.Forms.PictureBox()
+        Me.pbxPanic = New System.Windows.Forms.PictureBox()
+        Me.pbxOFF = New System.Windows.Forms.PictureBox()
+        Me.pbxHelp = New System.Windows.Forms.PictureBox()
+        Me.pbxLED = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pbxSupport, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxOnline, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxPanic, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxOFF, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxHelp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxLED, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbxSupport
+        '
+        Me.pbxSupport.Image = CType(resources.GetObject("pbxSupport.Image"), System.Drawing.Image)
+        Me.pbxSupport.Location = New System.Drawing.Point(101, 36)
+        Me.pbxSupport.Name = "pbxSupport"
+        Me.pbxSupport.Size = New System.Drawing.Size(20, 17)
+        Me.pbxSupport.TabIndex = 96
+        Me.pbxSupport.TabStop = False
+        Me.pbxSupport.Visible = False
+        '
+        'pbxOnline
+        '
+        Me.pbxOnline.Image = CType(resources.GetObject("pbxOnline.Image"), System.Drawing.Image)
+        Me.pbxOnline.Location = New System.Drawing.Point(75, 36)
+        Me.pbxOnline.Name = "pbxOnline"
+        Me.pbxOnline.Size = New System.Drawing.Size(20, 17)
+        Me.pbxOnline.TabIndex = 95
+        Me.pbxOnline.TabStop = False
+        Me.pbxOnline.Visible = False
+        '
+        'pbxPanic
+        '
+        Me.pbxPanic.Image = CType(resources.GetObject("pbxPanic.Image"), System.Drawing.Image)
+        Me.pbxPanic.Location = New System.Drawing.Point(52, 36)
+        Me.pbxPanic.Name = "pbxPanic"
+        Me.pbxPanic.Size = New System.Drawing.Size(17, 17)
+        Me.pbxPanic.TabIndex = 94
+        Me.pbxPanic.TabStop = False
+        Me.pbxPanic.Visible = False
+        '
+        'pbxOFF
+        '
+        Me.pbxOFF.Image = CType(resources.GetObject("pbxOFF.Image"), System.Drawing.Image)
+        Me.pbxOFF.Location = New System.Drawing.Point(30, 36)
+        Me.pbxOFF.Name = "pbxOFF"
+        Me.pbxOFF.Size = New System.Drawing.Size(17, 17)
+        Me.pbxOFF.TabIndex = 93
+        Me.pbxOFF.TabStop = False
+        Me.pbxOFF.Visible = False
+        '
+        'pbxHelp
+        '
+        Me.pbxHelp.Image = CType(resources.GetObject("pbxHelp.Image"), System.Drawing.Image)
+        Me.pbxHelp.Location = New System.Drawing.Point(7, 36)
+        Me.pbxHelp.Name = "pbxHelp"
+        Me.pbxHelp.Size = New System.Drawing.Size(17, 17)
+        Me.pbxHelp.TabIndex = 92
+        Me.pbxHelp.TabStop = False
+        Me.pbxHelp.Visible = False
+        '
+        'pbxLED
+        '
+        Me.pbxLED.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.pbxLED.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pbxLED.Image = CType(resources.GetObject("pbxLED.Image"), System.Drawing.Image)
+        Me.pbxLED.Location = New System.Drawing.Point(0, 0)
+        Me.pbxLED.Name = "pbxLED"
+        Me.pbxLED.Size = New System.Drawing.Size(26, 23)
+        Me.pbxLED.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbxLED.TabIndex = 91
+        Me.pbxLED.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'UCLed
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.pbxSupport)
+        Me.Controls.Add(Me.pbxOnline)
+        Me.Controls.Add(Me.pbxPanic)
+        Me.Controls.Add(Me.pbxOFF)
+        Me.Controls.Add(Me.pbxHelp)
+        Me.Controls.Add(Me.pbxLED)
+        Me.Name = "UCLed"
+        Me.Size = New System.Drawing.Size(26, 23)
+        CType(Me.pbxSupport, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxOnline, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxPanic, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxOFF, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxHelp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxLED, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Private WithEvents pbxSupport As System.Windows.Forms.PictureBox
+    Private WithEvents pbxOnline As System.Windows.Forms.PictureBox
+    Private WithEvents pbxPanic As System.Windows.Forms.PictureBox
+    Private WithEvents pbxOFF As System.Windows.Forms.PictureBox
+    Private WithEvents pbxHelp As System.Windows.Forms.PictureBox
+    Private WithEvents pbxLED As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+End Class
---
+++ UCLed.vb
@@ -0,0 +1,229 @@
+﻿Imports System.Drawing
+Imports System.Windows.Forms.Design
+Imports System.Windows.Forms
+
+Public Class UCLed
+
+    Public Enum TextAlign
+        MiddleLeft = 0
+        MiddleRight = 1
+    End Enum
+
+    Public Enum Type
+        Panic = 2
+        Help = 4
+        Support = 3
+        Online = 1
+    End Enum
+
+    Public Enum Status
+        Encendido = 1
+        Apagado = 0
+    End Enum
+
+    Private _Status As Status = Status.Apagado
+    Private _Tipo As Type = Type.Online
+    Private _TextAlign As TextAlign = TextAlign.MiddleLeft
+    Private _Text As String = "[Insert Text Here]"
+    Private _X1 As Integer
+    Private _X2 As Integer
+    Private _IsBold As Boolean = False
+    Public _IsRunning As Boolean = False
+    Private Sub UCWeb_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Timer1.Interval = 400
+        TxtAlign = _TextAlign
+    End Sub
+    Public Sub Start()
+        Timer1.Enabled = True
+    End Sub
+    Public Sub Pause()
+        Estado = Status.Apagado
+        Timer1.Enabled = False
+    End Sub
+    Private _uflag As SByte
+    Public Sub ShutDownEffect(ByVal sender As Object, ByVal e As EventArgs) Handles Timer1.Tick
+
+        If (IsRunning = True) Then
+            If (_uflag = 1) Then
+                pbxLED.Image = pbxOnline.Image
+                _uflag = 0
+            Else
+                pbxLED.Image = pbxOFF.Image
+                _uflag = 1
+            End If
+        End If
+    End Sub
+    Private Sub UCWeb_Resize(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Resize
+
+        Me.ResizeCtrls(_X1, _X2)
+    End Sub
+    ''' <summary>Alignment of the text displayed</summary>'[Browsable(true)][DisplayName("TextAlign")][Description("The Alignment of the text displayed")]
+    Public Property TxtAlign As TextAlign
+        Get
+            Return _TextAlign
+        End Get
+        Set(ByVal value As TextAlign)
+
+            _TextAlign = value
+
+            If (_TextAlign = TextAlign.MiddleLeft) Then
+                ResizeCtrls(0, pbxLED.Width)
+            End If
+
+            If (_TextAlign = TextAlign.MiddleRight) Then
+                'ResizeCtrls(lbDescription.Width, 0)
+            End If
+        End Set
+    End Property
+    '''<summary>The Text Displayed (Caption) of LED.</summary>'[Browsable(true)][DisplayName("TextDisplayed")][Description("The Text Displayed")]
+    Public Property Texto As String
+        Get
+            Return _Text
+        End Get
+        Set(ByVal value As String)
+            _Text = value
+            ' lbDescription.Text = _Text.Trim()
+            ' TxtAlign = _TextAlign ' //Resize Ctrls
+        End Set
+    End Property
+    '''<summary>Determina si el proceso de apagado/encendido se encuentra ejecutando</summary>
+    Public Property IsRunning As Boolean
+        Get
+            Return _IsRunning
+        End Get
+        Set(ByVal value As Boolean)
+            _IsRunning = value
+        End Set
+    End Property
+    '''<summary>Tipo de Encendido</summary> '[Browsable(true)][DisplayName("Type")][Description("Tipo de Encendido")]
+    Public Property Tipo As Type
+        Get
+            Return _Tipo
+        End Get
+        Set(ByVal value As Type)
+            _Tipo = value
+            If (Estado = Status.Encendido) Then
+                Select Case (_Tipo)
+                    Case Type.Help : pbxLED.Image = pbxHelp.Image
+                    Case Type.Online : pbxLED.Image = pbxOnline.Image
+                    Case Type.Panic : pbxLED.Image = pbxPanic.Image
+                    Case Type.Support : pbxLED.Image = pbxSupport.Image
+                    Case Else
+                        pbxLED.Image = pbxOnline.Image
+                End Select
+            End If
+        End Set
+    End Property
+
+    '''<summary>Estado del LED (Prendido,Apagado) </summary>'[Browsable(true)][DisplayName("Status")][Description("Estado del LED (Prendido,Apagado)")]
+    Public Property Estado As Status
+        Get
+            Return _Status
+        End Get
+        Set(ByVal value As Status)
+            _Status = value
+            If (_Status = Status.Apagado) Then
+                pbxLED.Image = pbxOFF.Image
+            Else 'Status.ON
+                Select Case (_Tipo)
+                    Case Type.Help : pbxLED.Image = pbxHelp.Image
+                    Case Type.Online : pbxLED.Image = pbxOnline.Image
+                    Case Type.Panic : pbxLED.Image = pbxPanic.Image
+                    Case Type.Support : pbxLED.Image = pbxSupport.Image
+                End Select
+            End If
+        End Set
+    End Property
+#Region "Methods"
+    Private Sub ResizeCtrls()
+
+        pbxLED.Location = New Point(_X1, 0)
+        'lbDescription.Location = New Point(_X2, 1) ' //19, 1 
+        ' Me.Size = New Size(pbxLED.Width + lbDescription.Width, pbxLED.Height) ' //MaxSize on Resize
+
+    End Sub
+    Private Sub ResizeCtrls(ByVal X1 As Integer, ByVal X2 As Integer)
+
+        pbxLED.Location = New Point(X1, 0)
+        ' lbDescription.Location = New Point(X2, 1) '//19, 1 
+        _X1 = X1 : _X2 = X2
+        ' Me.Size = New Size(pbxLED.Width + lbDescription.Width, pbxLED.Height) ' //MaxSize on Resize
+    End Sub
+#End Region
+
+End Class
+
+Public Class CDesigner
+    Inherits ControlDesigner
+
+    Protected Overrides Sub PreFilterEvents(ByVal events As System.Collections.IDictionary)
+
+        '    ''' <summary></summary> 
+        'Public Event ICheckAll(ByVal Bval As Boolean)
+
+        ' ''' <summary></summary> 
+        'Public Event ICellDoubleClick(ByVal sender As Object, ByVal e As DataGridViewCellEventArgs)
+
+        ' ''' <summary></summary> 
+        'Public Event ICellCntentClick(ByVal sender As Object, ByVal e As DataGridViewCellEventArgs)
+
+        ' ''' <summary></summary> 
+        'Public Event IConfirmed(ByVal Bval As Boolean)
+
+        ' ''' <summary>Evento Disparado al seleccionar un Registro de forma automatica.</summary> 
+        'Public Event ISelectedRow(ByVal Index As Integer)
+
+        ' ''' <summary>Evento Disparado cada vez que se Elimina un Registro</summary> 
+        'Public Event ISuccess()
+
+        ' ''' <summary>Evento disparada cada vez que se carga el archivo tfm</summary> 
+        'Public Event IUpdated()
+
+        Try
+            Dim ListEvents As List(Of DictionaryEntry) = events.Cast(Of DictionaryEntry).ToList
+            Dim Events_Exceptions() As String = {"ICheckAll", "ICellDoubleClick", "ICellCntentClick", "IConfirmed", "ISelectedRow", "ISuccess", "IUpdated"}
+
+            Dim EventName As String = ""
+
+            'Seek Property Name in the List of Property Exceptions
+            For Each EventFromList As DictionaryEntry In ListEvents
+                EventName = EventFromList.Key.ToString.Trim
+
+                If (Not Events_Exceptions.Any(Function(_PropertyName) EventName = _PropertyName)) Then
+                    events.Remove(EventFromList.Key.ToString)
+                End If
+            Next
+            '
+            MyBase.PreFilterEvents(events)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "PostFilterProperties", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        'events.Clear()
+    End Sub
+
+    Protected Overrides Sub PostFilterProperties(ByVal properties As System.Collections.IDictionary)
+
+        Try
+            Dim ListProperties As List(Of DictionaryEntry) = properties.Cast(Of DictionaryEntry).ToList
+            Dim Properties_Exceptions() As String = {"Name", "Size", "Location", "Tipo", "Estado", "Remark", "TxtAlign"}
+
+            Dim ProperyName As String = ""
+
+            'Seek Property Name in the List of Property Exceptions
+            For Each PropertyFromList As DictionaryEntry In ListProperties
+                ProperyName = PropertyFromList.Key.ToString.Trim
+
+                If (Not Properties_Exceptions.Any(Function(_PropertyName) ProperyName =
+                     _PropertyName OrElse ProperyName.StartsWith(_PropertyName & "_"))) Then
+                    properties.Remove(PropertyFromList.Key.ToString)
+                End If
+            Next
+            '
+            MyBase.PostFilterProperties(properties) 'Save Changes
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "PostFilterProperties", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+End Class
